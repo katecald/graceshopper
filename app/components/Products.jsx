d09abd11942d685fc55a335ab014fa59f2730583
Keziyah <keziyahlewis@gmail.com>
2017-04-17 18:15:03 -0400

    wtf

---
+++ app/components/Products.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+  // componentDidMount() {
+  //   this.nextJoke()
+  // }
+
+
+  render() {
+    return (
+      <div>
+          <h1>Holiday Helper</h1>
+          <h3>Because holidays don't need to be awful.</h3>
+      </div>
+    )
+  }
+}
+
+
+
